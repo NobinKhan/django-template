9491197634b237204034ef3d42a5882ee53ec783
nabinkhan <nazrulkhan.dk@gmail.com>
2023-10-23 10:31:37 +0600

    Fix: formatted

--- settings/configs/local.py
+++ settings/configs/local.py
@@ -1,11 +1,11 @@
-from settings.configs.base import * # noqa: F403
+from settings.configs.base import *  # noqa: F403
 from settings.configs.env import BASE_DIR
 
 # installed apps and middleware
 INSTALLED_APPS.append(  # noqa: F405
     "debug_toolbar",
 )
-
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
 MIDDLEWARE.append(  # noqa: F405
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 )
@@ -21,8 +21,13 @@ ALLOWED_HOSTS = ["*"]
 
 # media files config
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")    # noqa: F405
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # noqa: F405
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")  # noqa: F405
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
--- settings/servers/asgi.py
+++ settings/servers/asgi.py
@@ -1,24 +1,9 @@
 import os
-from pathlib import Path
 from django.core.asgi import get_asgi_application
 
-import environ
-
-# Set the project Environment
-ENVIRONMENT = os.environ.get("DJANGO_ENVIRONMENT")
-if not ENVIRONMENT:
-    raise ImportError({"Django": "Emproperly Configured System."})
-
-# Set the project base directory
-BASE_DIR = os.environ.get("DJANGO_BASE_DIR")
-if not BASE_DIR:
-    BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-    # Take environment variables from .env file
-    environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+from settings.configs.env import *  # noqa: F403
 
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"settings.configs.{ENVIRONMENT}")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"settings.configs.{ENVIRONMENT}")  # noqa: F405
 
 application = get_asgi_application()
